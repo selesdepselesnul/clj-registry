f0cfcdaf4b1c7d897b855023d09e19967a91fb3d
Moch Deden <moch.deden.r@gmail.com>
2018-01-19 13:06:52 +0700

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-registry
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_registry/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-registry.core-test
+  (:require [clojure.test :refer :all]
+            [clj-registry.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
